58ee3b6fdd370722d2cc989900f04a12d49aa549
Will Robinson <sumodude529@gmail.com>
2017-06-05 20:33:26 -0500

    Finished index.html, adding four sections, a name, and a profile picture

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    port = Number(process.env.PORT || 8888),
+    router = require('./routeHandler')
+
+var app = express()
+
+app.set('views', path.join(__dirname, '/views'))
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/', router)
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port)
+})
---
+++ routeHandler.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+    path = require('path'),
+    router = express.Router()
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+module.exports = router
