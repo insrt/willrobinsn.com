6b0a911e2283902efc5a4bfa8787bccfd7eed65c
Will Robinson <sumodude529@gmail.com>
2017-07-27 21:52:27 -0500

    changed link focus color

--- routeHandler.js
+++ routeHandler.js
@@ -6,15 +6,15 @@ var express = require('express'),
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 })
-
-router.get('/blog', blog.getAll)
-
-router.get('/blog/create', (req, res) => { /*finished*/
-  res.sendFile(path.join(__dirname, '/views/newPost.html'))
-})
-router.post('/blog', blog.create) /*finished*/
-router.get('/blog/:id', blog.get)  /*finished everything except the view*/
-router.put('/blog/:id', blog.update)
-router.delete('/blog/:id', blog.delete)
-
-module.exports = router
+// 
+// router.get('/blog', blog.getAll)
+//
+// router.get('/blog/create', (req, res) => { /*finished*/
+//   res.sendFile(path.join(__dirname, '/views/newPost.html'))
+// })
+// router.post('/blog', blog.create) /*finished*/
+// router.get('/blog/:id', blog.get)  /*finished everything except the view*/
+// router.put('/blog/:id', blog.update)
+// router.delete('/blog/:id', blog.delete)
+//
+// module.exports = router
